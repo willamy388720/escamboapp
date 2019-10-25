a92f1c574b72d21a30cac9350fb2082302a2290f
willamy388720 <willamygt@gmail.com>
2019-10-25 14:57:00 -0300

    Modal dinamico parte 2/2

--- app/controllers/backoffice/send_mail_controller.rb
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -1,2 +1,12 @@
 class Backoffice::SendMailController < ApplicationController
+    def edit
+        @admin = Admin.find(params[:id])
+        respond_to do |format|
+            format.js
+        end
+    end
+
+    def create
+        #
+    end
 end
