0941387dc082f1c461c39405347506e4e0a3bce2
willamy388720 <willamygt@gmail.com>
2020-03-23 16:10:26 -0300

    Respondendo Json e XML

--- app/controllers/site/ad_detail_controller.rb
+++ app/controllers/site/ad_detail_controller.rb
@@ -2,5 +2,11 @@ class Site::AdDetailController < SiteController
     def show
         @categories = Category.order_by_description
         @ad = Ad.find(params[:id])
+
+        # respond_to do |format|
+        #     format.html
+        #     format.json { render json: @ad }
+        #     format.xml { render xml: @ad }
+        # end
     end
 end
