0685f6ffaf5e25272596776968eccbca8faf4ad2
willamy388720 <willamygt@gmail.com>
2020-03-24 15:44:55 -0300

    Sessions

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,6 +1,8 @@
 class Site::HomeController < SiteController
   
   def index
+    
+
     @categories = Category.order_by_description
     @ads = Ad.descending_order(params[:page])
     @carousel = Ad.random(3)
