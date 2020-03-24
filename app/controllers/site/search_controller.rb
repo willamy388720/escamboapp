c8e74e8db6d82a4e393c04f3e01e5b44b38dd111
willamy388720 <willamygt@gmail.com>
2020-03-24 15:34:50 -0300

    Cookies

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,6 +1,10 @@
 class Site::SearchController < SiteController
     def ads
+        cookies[:search_term] = params[:q]
+
         @ads = Ad.search(params[:q], params[:page])
         @categories = Category.all
+
+        cookies[:categories] = @categories.to_json
     end
 end
